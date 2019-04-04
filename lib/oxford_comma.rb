8301d28571d37eaa78b8c0e0de0ddba5dc9ec7df
Jake L <jakelovitz@gmail.com>
2019-04-04 14:56:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length == 3
+   return array[0..1].join(", ") + ", and " + array.last
+  elsif array.length > 3
+    return array[0..-2].join(", ") + ", and " + array.last
+  end
 end
