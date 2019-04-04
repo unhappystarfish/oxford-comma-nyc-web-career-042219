1d54a56545e91b323b0d2178b9ced30af1bd2a0f
Jake L <jakelovitz@gmail.com>
2019-04-04 14:43:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length == 3
    return array[0..1].join(", ") + ", and " + array.last
+  elsif array.length > 3
+    return array
   end
 end
